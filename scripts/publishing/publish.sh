1339aec1ca1c96b8521fa33ee86fd83a6adfff8d
Administrator <Administrator@instance-o0o3oq>
2021-06-06 18:33:30 +0800

    changed output format and the ZIP location to the webhelp

--- scripts/publishing/publish.sh
+++ scripts/publishing/publish.sh
@@ -5,30 +5,30 @@ if [[ ! -e bin/dita-ot.zip ]];
 then
   mkdir bin
   
-  curl -q -L https://github.com/dita-ot/dita-ot/releases/download/3.3.3/dita-ot-3.3.3.zip --output bin/dita-ot.zip
+  curl -q -L https://github.com/dcwise/dita-docs-as-code-RAYTECH/tree/master/source/dita-ot-3.6.1.zip --output bin/dita-ot.zip
   
   ls -l bin/dita-ot.zip
   
   unzip -q bin/dita-ot.zip -d bin/
 fi
 
-echo "====================================="
-echo "download WebHelp plugin"
-echo "====================================="
+#echo "====================================="
+#echo "download WebHelp plugin"
+#echo "====================================="
 
-curl -q -L https://mirror.oxygenxml.com/InstData/WebHelp/oxygen-webhelp-dot-3.x.zip  --output bin/oxygen-webhelp-dot-2.x.zip
+#curl -q -L https://mirror.oxygenxml.com/InstData/WebHelp/oxygen-webhelp-dot-3.x.zip  --output bin/oxygen-webhelp-dot-2.x.zip
 
 
-echo "====================================="
-echo "extract WebHelp to DITA-OT"
-echo "====================================="
-unzip -q bin/oxygen-webhelp-dot-2.x.zip -d bin/
+#echo "====================================="
+#echo "extract WebHelp to DITA-OT"
+#echo "====================================="
+#unzip -q bin/oxygen-webhelp-dot-2.x.zip -d bin/
 
-cp -R bin/com.oxygenxml.* $DITA_OT/plugins/
+#cp -R bin/com.oxygenxml.* $DITA_OT/plugins/
 
-pwd
+#pwd
 
-cp scripts/publishing/licensekey.txt $DITA_OT/plugins/com.oxygenxml.webhelp.responsive/licensekey.txt
+#cp scripts/publishing/licensekey.txt $DITA_OT/plugins/com.oxygenxml.webhelp.responsive/licensekey.txt
 
 echo "====================================="
 echo "Add Edit Link to DITA-OT"
@@ -62,6 +62,6 @@ cd ../..
 pwd
 
 sh $DITA_OT/bin/dita \
-    --format=markdown_gitbook \
+    --format=webhelp-responsive \
     --input=source/markdown-dita/garage.ditamap \
     --output=bin/out
